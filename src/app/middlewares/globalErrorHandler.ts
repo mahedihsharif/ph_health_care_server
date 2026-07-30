e614a785695c626a7a8b88019294d462cbf6bdef
mahedihsharif <mahedi5061@gmail.com>
2026-07-30 20:51:04 +0600

    handle-erros

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,18 +1,34 @@
-import { NextFunction, Request, Response } from "express"
-import httpStatus from "http-status"
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import { Prisma } from "../../../generated/prisma/client";
 
-const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+const globalErrorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  let statusCode: number = httpStatus.INTERNAL_SERVER_ERROR;
+  let success = false;
+  let message = err.message || "Something went wrong!";
+  let error = err;
 
-    let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
-    let success = false;
-    let message = err.message || "Something went wrong!";
-    let error = err;
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === "P2002") {
+      message = "Unique constraint failed";
+      error = err.meta;
+      statusCode = httpStatus.CONFLICT;
+    }
+  } else if (err instanceof Prisma.PrismaClientValidationError) {
+    message = "Validation Error";
+    statusCode = httpStatus.BAD_REQUEST;
+  }
 
-    res.status(statusCode).json({
-        success,
-        message,
-        error
-    })
+  res.status(statusCode).json({
+    success,
+    message,
+    error,
+  });
 };
 
 export default globalErrorHandler;
